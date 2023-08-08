5eddcb4e4372141ef5df48ce53f5b3db3796f041
Anish Gupta <anishgupta675@gmail.com>
2023-08-08 21:38:24 +0530

    some commit

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+// import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.css']
 })
+
 export class SignupComponent {
 
 }
+
+// export class SignupComponent implements OnInit {
+//   signupForm: FormGroup;
+
+//   constructor() {}
+
+//   ngOnInit() {
+//     this.signupForm = new FormGroup({
+//       username: new FormControl('', Validators.required),
+//       email: new FormControl('', [Validators.required, Validators.email]),
+//       password: new FormControl('', Validators.required),
+//     });
+//   }
+// }
